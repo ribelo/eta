672ddd188bdebe94ffaafe1d42d832765f6a1861
Rafał Krzyważnia <r.krzywaznia@gmail.com>
2026-07-11 14:24:24 +0200

    refactor: remove shallow exit helpers

--- lib/eta/exit.ml
+++ lib/eta/exit.ml
@@ -2,26 +2,8 @@ type ('a, 'err) t =
   | Ok of 'a
   | Error of 'err Cause.t
 
-let ok value = Ok value
-let error cause = Error cause
-
-let is_ok = function Ok _ -> true | Error _ -> false
-let is_error exit = not (is_ok exit)
-let get_success = function Ok value -> Some value | Error _ -> None
-let get_cause = function Ok _ -> None | Error cause -> Some cause
-let match_ ~ok ~error = function Ok value -> ok value | Error cause -> error cause
 let map f = function Ok value -> Ok (f value) | Error cause -> Error cause
 let map_error f = function Ok value -> Ok value | Error cause -> Error (Cause.map f cause)
-
-let map_both ~ok ~error = function
-  | Ok value -> Ok (ok value)
-  | Error cause -> Error (Cause.map error cause)
-
-let get_or_else on_error = function
-  | Ok value -> value
-  | Error cause -> on_error cause
-
-let as_unit = function Ok _ -> Ok () | Error cause -> Error cause
 
 let to_result = function
   | Ok value -> Some (Stdlib.Ok value)
--- lib/eta/exit.mli
+++ lib/eta/exit.mli
@@ -4,28 +4,8 @@ type ('a, 'err) t =
   | Ok of 'a
   | Error of 'err Cause.t
 
-val ok : 'a -> ('a, 'err) t
-val error : 'err Cause.t -> ('a, 'err) t
-
-val is_ok : ('a, 'err) t -> bool
-val is_error : ('a, 'err) t -> bool
-val get_success : ('a, 'err) t -> 'a option
-val get_cause : ('a, 'err) t -> 'err Cause.t option
-
-val match_ :
-  ok:('a -> 'b) -> error:('err Cause.t -> 'b) -> ('a, 'err) t -> 'b
-
 val map : ('a -> 'b) -> ('a, 'err) t -> ('b, 'err) t
 val map_error : ('err1 -> 'err2) -> ('a, 'err1) t -> ('a, 'err2) t
-
-val map_both :
-  ok:('a -> 'b) ->
-  error:('err1 -> 'err2) ->
-  ('a, 'err1) t ->
-  ('b, 'err2) t
-
-val get_or_else : ('err Cause.t -> 'a) -> ('a, 'err) t -> 'a
-val as_unit : ('a, 'err) t -> (unit, 'err) t
 
 val to_result : ('a, 'err) t -> ('a, 'err) result option
 (** Convert to OCaml [result] only when the exit is [Ok _] or a single
